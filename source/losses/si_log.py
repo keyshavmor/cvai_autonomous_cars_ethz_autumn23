eff03d42ad9045f2ef3c4040a2d07ac98e4094be
Shun_Takashige <shigeshigesyun94@gmail.com>
2023-10-31 21:42:43 +0100

    update for si_log

--- source/losses/si_log.py
+++ source/losses/si_log.py
@@ -30,10 +30,16 @@ class SILogLoss(torch.nn.Module):
             input = input.squeeze(1)
         if target.ndim == 4:
             target = target.squeeze(1)
-        
+            
 
         # TODO: Implement a proper silog loss and taking into consideration
         # the invalid pixels (target is 0).
-        loss = (input - target).abs().sum()
+        K = 0.15
+        mask = (target > 0)
+        d = torch.log(input[mask]) - torch.log(target[mask])
+        return (d**2).mean() - K * (d.mean())**2
+        
+        # loss = (input - target).sum()**2
+        
 
         return loss
